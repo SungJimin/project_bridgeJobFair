68d2bc97e43cbb9ef861c9c254e3925a63a88112
Jimin Sung <50706377+SungJimin@users.noreply.github.com>
2022-12-09 13:37:00 +0900

    미국인턴십 기업 이미지 및 텍스트 수정 v2

--- js/index.js
+++ js/index.js
@@ -1,38 +1,39 @@
 // index.js
-$(function(){
+$(function () {
 
-    //MAin Content 슬라이딩 배너
-    var swiper = new Swiper('.swiper-container', {
-      centeredSlides: true,
-      autoplay: {
-        delay: 2500,
-        disableOnInteraction: false,
-      },
-      speed: 2000,
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-      }
-    });
-
-    //header>.top-menu Language Event 호버시 드랍 메뉴
-    var $nav = $('ul.user-menu'),
+  //header>.top-menu Language Event 호버시 드랍 메뉴
+  var $nav = $('ul.user-menu'),
     $navWrap = $('ul.user-menu .navWrap'),
-        $gnb = $('ul.user-menu .gnb'),
-        $lnb = $('ul.user-menu .lnb');
+    $gnb = $('ul.user-menu .gnb'),
+    $lnb = $('ul.user-menu .lnb');
 
-    $lnb.css('display','none');
+  $lnb.css('display', 'none');
 
-    $gnb.mouseenter(function(){
-        var dp = $(this).next().css('display');
-        if(dp == 'none'){
-            $lnb.slideUp();
-            $(this).next().slideDown();
-        }
-    });
+  $gnb.mouseenter(function () {
+    var dp = $(this).next().css('display');
+    if (dp == 'none') {
+      $lnb.slideUp();
+      $(this).next().slideDown();
+    }
+  });
 
-    $navWrap.mouseleave(function(){
-        $lnb.slideUp();
-    });
+  $navWrap.mouseleave(function () {
+    $lnb.slideUp();
+  });
 
+  // 참가기업 List(List 8개 이상일 때 마지막 요소에 클래스 추가하기)
+  var $listWrap = $('.fairListWrap .fairList');
+  
+  $listWrap.each(function (index, item) {
+    var list = $(item).children();
+    if (list.length >= 8) {
+      var last = list[7];
+      $(last).addClass("last")
+    }
+  });
 });
+
+// function findLastList(){
+//   let listWrap = $('');
+//   let last
+// }
